b251b6ec69166d88bf33e9cbcf663dbbc4946722
Kai Kuspa <kuspakai@stanford.edu>
2017-03-11 21:39:09 -0800

    doc2vec wordbindings for microsoft

---
+++ make_doc2vec.py
@@ -0,0 +1,7 @@
+from gensim import utils
+from gensim.models.doc2vec import LabeledSentence
+from gensim.models import Doc2Vec
+import numpy
+from random import shuffle
+from sklearn.linear_model import LogisticRegression
+
