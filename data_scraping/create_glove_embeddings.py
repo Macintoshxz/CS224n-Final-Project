4abc3009931cabfbe70a2582f40c1a20992a1163
Victor Cheung <victorcheung@uchicago.edu>
2017-03-20 23:27:39 -0700

    updated create glove embeddings to allow for integer id mappings

--- data_scraping/create_glove_embeddings.py
+++ data_scraping/create_glove_embeddings.py
@@ -8,10 +8,58 @@ import string
 import os
 from functools import partial
 import pickle
+from collections import OrderedDict
 
 replace_punctuation = string.maketrans(string.punctuation, ' '*len(string.punctuation))
 replace_digits = string.maketrans(string.digits, ' '*len(string.digits))
 
+# Helper function for processing filenames into (ticker, year, section)
+# Based on assumption that if year, month, day <= June 30 then is year - 1 else year 
+
+def parseFilename(filename):
+	s = filename.split(".")[0].split("_")
+	section = s[-1]
+	ticker = s[0]
+	year, month, day = s[1].split('-')
+	year = int(year)
+	if month <= "06" and day <= "30" and year != "1994":
+		year = year - 1
+	return (ticker, year, section)
+
+
+# Return the integer id representation of a paragraph of words
+# according to the order of GloveDict of word : embedding. 
+
+def paragraphWordID(s, dict):
+    s = s.lower()
+    s = s.translate(replace_punctuation)
+    s = s.translate(replace_digits)
+    s = re.sub(r'\s+', ' ', s)
+
+    tokens = s.split(' ')
+    numTokens = len(tokens)
+
+    # tokenVals = []
+    # for token in tokens:
+    # 	if token in dict:
+    # 		print "appending"
+    # 		tokenVals.append(dict[token])
+
+    # Skips unknown words in the glove dictionary
+    tokenVals = np.array([dict[token] for token in tokens if token in dict])
+    return tokenVals
+
+
+def createDocumentWordIDMapping(targetPath, dict):
+	filename = targetPath.split('/')[-1]
+
+	file_10k = open(targetPath, 'r')
+	fileString = file_10k.read()
+	file_10k.close()
+
+	integerMapping = paragraphWordID(fileString, dict)
+
+	return [filename, integerMapping]
 
 # Actually formats, then converts a string to
 # an embedding.
@@ -32,7 +80,7 @@ def stringToEmbedding(s):
 # pass it a gloveDict to open the file.
 
 def createSingleEmbedding(targetPath):
-	filename = targetPath.split('\\')[-1]
+	filename = targetPath.split('/')[-1]
 
 	file_10k = open(targetPath, 'r')
 	fileString = file_10k.read()
@@ -55,10 +103,13 @@ def createSingleEmbedding(targetPath):
 
 class EmbeddingCreator():
 	# Class variable to be shared
-	global gloveDict
+	
 
-	def __init__(self):
+	def __init__(self, gloveDim):
 		self.hello = "Let's create some embeddings"
+		self.gloveDim = gloveDim
+		self.gloveDict = self.createGloveDict(self.gloveDim)
+		
 
 	# def calculateParallel(self, inputs, threads=1):
 	# 	pool = ThreadPool(threads)
@@ -81,8 +132,8 @@ class EmbeddingCreator():
 	def createGloveDict(self, gloveDim='50'):
 		glovePath = "glove/glove.6B." + gloveDim + 'd.txt'
 
-		global gloveDict
-		if not os.path.isfile('/glovegloveDict_' + gloveDim + '.pkl'):
+		
+		if not os.path.isfile('glove/gloveDict_' + gloveDim + '.pkl'):
 			print 'Creating new gloveDict'
 			newGloveDict = {}
 			gloveFile = open(glovePath)
@@ -95,7 +146,7 @@ class EmbeddingCreator():
 			    embedding = np.float32(split[1:])
 			    newGloveDict[word] = embedding
 
-			gloveDict = newGloveDict
+			gloveDict = OrderedDict(newGloveDict)
 			pickle.dump(gloveDict, open('glove/gloveDict_' + gloveDim + '.pkl', "wb+" ) )
 		else:
 			print 'Loading gloveDict'
@@ -112,12 +163,18 @@ class EmbeddingCreator():
 	# Manages calling of other functions.  Gets all appropriate files to embed,
 	# then decides which threading format to use to get the embedding dictionary.
 	# Saves the output into a pickle.
-	def createEmbeddings(self, gloveDim='50', path="SEC-Edgar-data/"):
-		startDict = time.time()
-		self.gloveDict = self.createGloveDict(gloveDim)
-		endDict = time.time()
-		print "Creating dict took:", endDict - startDict, "seconds."
-		embeddingDict = {}
+	#
+	# If doc = True, creates document embeddings via averaged glove vectors.
+	# If doc = False, creates word integer id mappings. 
+	def createEmbeddings(self, path="SEC-Edgar-data/", doc = True):
+		# startDict = time.time()
+		# self.gloveDict = self.createGloveDict(gloveDim)
+		# endDict = time.time()
+		# pickle.dump(self.gloveDict, open("glove_d" + gloveDim, "w"))
+		# print "Creating dict took:", endDict - startDict, "seconds."
+
+		# self.gloveDict = pickle.load(open("glove_d" + gloveDim, "r"))
+		
 		targetPaths = []
 		start = time.time()
 		for subdir, dirs, files in os.walk(path):
@@ -125,36 +182,85 @@ class EmbeddingCreator():
 				docPath = os.path.join(subdir, f)
 				targetPaths.append(docPath)
 
-		totalPaths = len(targetPaths)
-		# targetPaths = targetPaths[:100]
-		# print 'total paths: ', str(totalPaths)
-		# if num_threads == 1:
-		# 	print 'SINGLE THREAD'
-		counter = 0
-		results = [createSingleEmbedding(targetPath) for targetPath in targetPaths]
-		for result in results:
-			filename = result[0]
-			embedding = result[1]
-			embeddingDict[filename] = embedding
-			# for targetFile in targetPaths:
-			# 	if counter % 50 == 0:
-			# 		print '\nFinished', str(counter), '/', str(totalPaths), 'in', str(time.time() - start), '\n'
-			# 	# filename, embedding = createSingleEmbedding(self.gloveDict, targetFile)
-			# 	filename, embedding = createSingleEmbedding(targetFile)
-			# 	counter += 1
+		if doc == True:
+			embeddingDict = {}
+			totalPaths = len(targetPaths)
+			# targetPaths = targetPaths[:100]
+			# print 'total paths: ', str(totalPaths)
+			# if num_threads == 1:
+			# 	print 'SINGLE THREAD'
+			counter = 0
+			results = [createSingleEmbedding(targetPath) for targetPath in targetPaths if targetPath.split(".")[-1] == "txt"]
+			for result in results:
+				filename = result[0]
+				embedding = result[1]
+				ticker, year, section = parseFilename(filename)
+				embeddingDict[(ticker, year, section)] = embedding
+				# for targetFile in targetPaths:
+				# 	if counter % 50 == 0:
+				# 		print '\nFinished', str(counter), '/', str(totalPaths), 'in', str(time.time() - start), '\n'
+				# 	# filename, embedding = createSingleEmbedding(self.gloveDict, targetFile)
+				# 	filename, embedding = createSingleEmbedding(targetFile)
+				# 	counter += 1
 
-			# 	embeddingDict[filename] = embedding
-		# else:
-		# 	print 'MULTITHREAD'
-		# 	results = self.calculateParallel(targetPaths, num_threads)
-		# 	for result in results:
-		# 		for filename, embedding in result:
-		# 			embeddingDict[filename] = embedding
+				# 	embeddingDict[filename] = embedding
+			# else:
+			# 	print 'MULTITHREAD'
+			# 	results = self.calculateParallel(targetPaths, num_threads)
+			# 	for result in results:
+			# 		for filename, embedding in result:
+			# 			embeddingDict[filename] = embedding
 
-		pickle.dump(embeddingDict, open( "../embeddingDicts/embeddingDict_" + gloveDim + ".pkl", "wb+" ) )
-		end = time.time()
-		print '\n\n\n FINAL TIME:'
-		print end - start
+			pickle.dump(embeddingDict, open( "../embeddingDicts/embeddingDict_" + self.gloveDim + ".pkl", "wb+" ) )
+			end = time.time()
+			print '\n\n\n FINAL TIME:'
+			print end - start
+
+		else: 
+			print "Creating Integer Id Mapping for Words in Documents"
+			DocumentWordIDDict = {}
+			totalPaths = len(targetPaths)
+			# targetPaths = targetPaths[:100]
+			# print 'total paths: ', str(totalPaths)
+			# if num_threads == 1:
+			# 	print 'SINGLE THREAD'
+			counter = 0
+			results = []
+
+			gloveDictKeys = self.gloveDict.keys()
+			dict = {}
+			for i in range(len(gloveDictKeys)):
+				dict[gloveDictKeys[i]] = i
+
+			for targetPath in targetPaths:
+				if targetPath.split(".")[-1] == "txt" and "section" in targetPath.split("_"):
+					print "Working on " + targetPath
+					results.append(createDocumentWordIDMapping(targetPath, dict))
+			# results = [createDocumentWordIDMapping(targetPath) for targetPath in targetPaths]
+			for result in results:
+				filename = result[0]
+				wordsID = result[1]
+				ticker, year, section = parseFilename(filename)
+				DocumentWordIDDict[(ticker, year, section)] = wordsID
+				# for targetFile in targetPaths:
+				# 	if counter % 50 == 0:
+				# 		print '\nFinished', str(counter), '/', str(totalPaths), 'in', str(time.time() - start), '\n'
+				# 	# filename, embedding = createSingleEmbedding(self.gloveDict, targetFile)
+				# 	filename, embedding = createSingleEmbedding(targetFile)
+				# 	counter += 1
+
+				# 	embeddingDict[filename] = embedding
+			# else:
+			# 	print 'MULTITHREAD'
+			# 	results = self.calculateParallel(targetPaths, num_threads)
+			# 	for result in results:
+			# 		for filename, embedding in result:
+			# 			embeddingDict[filename] = embedding
+
+			pickle.dump(DocumentWordIDDict, open( "DocumentWordIDDict_" + self.gloveDim + ".pkl", "wb+" ) )
+			end = time.time()
+			print '\n\n\n FINAL TIME:'
+			print end - start
 
 	def testEmbeddings(self, gloveDim):
 		self.gloveDict = self.createGloveDict(gloveDim)
@@ -163,9 +269,20 @@ class EmbeddingCreator():
 		print stringToEmbedding(s1)
 		print stringToEmbedding(s2)
 
+	def testIntegerMapping(self, gloveDim):
+		mapping = pickle.load(open("DocumentWordIDDict_" + gloveDim + ".pkl", "r" ))
+		trial = mapping[('DATA', 2016, '8')]
+		list = self.gloveDict.keys()
+		print "length of word embedding is " + str(len(self.gloveDict[list[1]]))
+		print [list[intId] for intId in trial]
+
 	
 # argv[1] = gloveDim
 if __name__ == '__main__':
-	embeddingCreator = EmbeddingCreator()
-	embeddingCreator.createEmbeddings(sys.argv[1])
+	embeddingCreator = EmbeddingCreator(sys.argv[1])
+	# Doc = False for word integer id mapping; True for document embeddings
+	embeddingCreator.createEmbeddings(doc = False)
+
+	#Testing
+	# embeddingCreator.testIntegerMapping("300")
 	# embeddingCreator.testEmbeddings()
